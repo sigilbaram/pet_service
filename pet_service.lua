9f63605b69411b4e69bdcdb70c3f1e83ae7a17b0
Sigil Baram <sigilbaram@gmail.com>
2018-10-26 02:29:47 -0400

    Progress. Waiting on debugging in packet_service.

--- pet_service.lua
+++ pet_service.lua
@@ -1,13 +1,14 @@
 local packets = require('packets')
 local server = require('shared.server')
 local structs = require('structs')
+local math = require('math')
 
 local data = server.new(structs.struct({
-    index           = {structs.entity_index},
-    id              = {structs.entity},
+    index           = {structs.uint16},
+    id              = {structs.uint32},
     name            = {structs.string(0x10)},
-    owner_index     = {structs.entity_index},
-    owner_id        = {structs.entity},
+    owner_index     = {structs.uint16},
+    owner_id        = {structs.uint32},
     hp              = {structs.uint16},
     hp_max          = {structs.uint16},
     hp_percent      = {structs.uint16},
@@ -15,7 +16,7 @@ local data = server.new(structs.struct({
     mp_max          = {structs.uint16},
     mp_percent      = {structs.uint16},
     tp              = {structs.uint32},
-    target_id       = {structs.entity},
+    target_id       = {structs.uint32},
     active          = {structs.bool}
 }))
 
@@ -42,13 +43,19 @@ packets.incoming:register_init({
             data.name = p.pet_name
         end
     end,
-    [{0x044, 0x12}] = function(p)
-        data.name = p.pet_name
-        data.hp = p.hp
-        data.hp_max = p.hp_max
-        data.hp_percent = math.floor(100*hp/hp_max)
-        data.mp = p.mp
-        data.mp_max = p.mp_max
-        data.hp_percent = math.floor(100*mp/mp_max)
+    [{0x044,0x12}] = function(p)
+        if p.pet_name and p.pet_name ~= '' then
+            data.name = p.pet_name
+        end
+        if p.hp_max ~= 0 then
+            data.hp = p.hp
+            data.hp_max = p.hp_max
+            data.hp_percent = math.floor(100 * p.hp / p.hp_max)
+        end
+        if p.mp_max ~= 0 then
+            data.mp = p.mp
+            data.mp_max = p.mp_max
+            data.hp_percent = math.floor(100 * p.mp / p.mp_max)
+        end
     end
 })
