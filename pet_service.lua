438bf900e651f6f0c246cf226a9fefcff1e03583
Sigil Baram <sigilbaram@gmail.com>
2018-10-29 11:53:56 -0400

    Separated and expanded automaton specific fields

--- pet_service.lua
+++ pet_service.lua
@@ -1,3 +1,6 @@
+local jit = require('jit')
+jit.off()
+
 local packets = require('packets')
 local server = require('shared.server')
 local structs = require('structs')
@@ -9,15 +12,44 @@ local data = server.new(structs.struct({
     name            = {structs.string(0x10)},
     owner_index     = {structs.uint16},
     owner_id        = {structs.uint32},
-    hp              = {structs.uint16},
-    hp_max          = {structs.uint16},
     hp_percent      = {structs.uint16},
-    mp              = {structs.uint16},
-    mp_max          = {structs.uint16},
     mp_percent      = {structs.uint16},
     tp              = {structs.uint32},
     target_id       = {structs.uint32},
-    active          = {structs.bool}
+    active          = {structs.bool},
+    automaton       = {structs.struct({
+        head                = {structs.uint16},
+        frame               = {structs.uint16},
+        attachments         = {structs.uint16[0x0C]},
+        available_heads     = {structs.data(4)},
+        available_frames    = {structs.data(4)},
+        available_attach    = {structs.data(32)},
+        name                = {structs.string(0x10)},
+        hp                  = {structs.uint16},
+        hp_max              = {structs.uint16},
+        mp                  = {structs.uint16},
+        mp_max              = {structs.uint16},
+        melee               = {structs.uint16},
+        melee_max           = {structs.uint16},
+        ranged              = {structs.uint16},
+        ranged_max          = {structs.uint16},
+        magic               = {structs.uint16},
+        magic_max           = {structs.uint16},
+        str                 = {structs.uint16},
+        str_max             = {structs.uint16},
+        dex                 = {structs.uint16},
+        dex_max             = {structs.uint16},
+        vit                 = {structs.uint16},
+        vit_max             = {structs.uint16},
+        agi                 = {structs.uint16},
+        agi_max             = {structs.uint16},
+        int                 = {structs.uint16},
+        int_max             = {structs.uint16},
+        mnd                 = {structs.uint16},
+        mnd_max             = {structs.uint16},
+        chr                 = {structs.uint16},
+        chr_max             = {structs.uint16}
+    })}
 }))
 
 packets.incoming:register_init({
@@ -62,17 +94,44 @@ packets.incoming:register_init({
         end
     end,
     [{0x044,0x12}] = function(p)
+        data.automaton.head             = p.automaton_head
+        data.automaton.frame            = p.automaton_frame
+        data.automaton.attachments      = p.attachments
+        data.automaton.available_heads  = p.available_heads
+        data.automaton.available_frames = p.available_frames
+        data.automaton.available_attach = p.available_attach
+        data.automaton.hp               = p.hp
+        data.automaton.hp_max           = p.hp_max
+        data.automaton.mp               = p.mp
+        data.automaton.mp_max           = p.mp_max
+        data.automaton.melee            = p.melee
+        data.automaton.melee_max        = p.melee_max
+        data.automaton.ranged           = p.ranged
+        data.automaton.ranged_max       = p.ranged_max
+        data.automaton.magic            = p.magic
+        data.automaton.magic_max        = p.magic_max
+        data.automaton.str              = p.str
+        data.automaton.str_max          = p.str_max
+        data.automaton.dex              = p.dex
+        data.automaton.dex_max          = p.dex_max
+        data.automaton.vit              = p.vit
+        data.automaton.vit_max          = p.vit_max
+        data.automaton.agi              = p.agi
+        data.automaton.agi_max          = p.agi_max
+        data.automaton.int              = p.int
+        data.automaton.int_max          = p.int_max
+        data.automaton.mnd              = p.mnd
+        data.automaton.mnd_max          = p.mnd_max
+        data.automaton.chr              = p.chr
+        data.automaton.chr_max          = p.chr_max
+
         if p.pet_name and p.pet_name ~= '' then
             data.name = p.pet_name
         end
         if p.hp_max ~= 0 then
-            data.hp = p.hp
-            data.hp_max = p.hp_max
             data.hp_percent = math.floor(100 * p.hp / p.hp_max)
         end
         if p.mp_max ~= 0 then
-            data.mp = p.mp
-            data.mp_max = p.mp_max
             data.mp_percent = math.floor(100 * p.mp / p.mp_max)
         end
     end
